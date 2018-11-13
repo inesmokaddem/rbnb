b0184edce71ca8e83e8b301f334984df8cca4562
Diego Bolettieri <diego@bebetterhotels.com>
2018-11-13 17:14:33 +0100

    finished form for artist#new and modified ArtistsControllers

--- app/models/public.rb
+++ app/models/public.rb
@@ -1,4 +1,7 @@
 class Public < ApplicationRecord
   has_many :artists
-  validates :label, inclusion: { in: ['Tous public', 'Enfants', 'Adolescents', 'Adultes']}
+  validates :label, inclusion: { in: ['Tous public', 'Enfants', 'Adolescents', 'Adultes'] }
+  def to_s
+    label
+  end
 end
