f7d6068d76c4f2c7c2b27384cdada60f21d99b5a
Diego Bolettieri <diego@bebetterhotels.com>
2018-11-15 19:00:55 +0100

    finished routing, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   root to: 'artists#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :artists, only: [:show, :new, :create], shallow: true do
-    resources :bookings, only: [:show, :create]  do
-      resources :reviews, only: [:create]
-    end
+    resources :bookings, only: [:show, :create]
   end
+  patch "bookings/:id", to: "reviews#create"
   resources :bookings, only: [:index, :destroy]
 end
