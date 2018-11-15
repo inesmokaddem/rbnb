fca999ac9c7fe47d7f3ed1da2f6b387df3e839ab
Diego Bolettieri <diego@bebetterhotels.com>
2018-11-15 17:24:55 +0100

    added ReviewsController, Views and modified ArtistController

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,13 @@
 class ReviewsController < ApplicationController
-
-
   def create
-
+    @booking = Booking.find(params[:id])
+    @booking.update(review_params)
+    redirect_to artist_path(@booking.artist)
   end
 
+  private
 
+  def review_params
+    params.require(:booking).permit(:review, :rating)
+  end
 end
