fd1834a48e88aa3b0eeb615acea0519fe395897c
inesmokaddem <ines.mokaddem@gmail.com>
2018-11-15 18:41:52 +0100

    initialisation geoloc

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -9,4 +9,7 @@ class Artist < ApplicationRecord
   validates :description, presence: true
   validates :address, presence: true
   validates :hour_rate, presence: true, numericality: {only_integer: true}
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
