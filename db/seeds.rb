c60588fb198d53bacb09099a4c76040bc84b0f54
inesmokaddem <ines.mokaddem@gmail.com>
2018-11-15 12:48:53 +0100

    seed modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 require 'faker'
 
 # puts "Destroy User"
-# User.destroy_all
 #
 # puts "Destroy Artists"
 Artist.destroy_all
@@ -11,10 +10,11 @@ Public.destroy_all
 #
 # puts "Destroy categories"
 Category.destroy_all
+User.destroy_all
 #
 
 
-# user = User.create!(email: "ines@gmail.com", password: "secret")
+user = User.create!(email: "diego@gmail.com", password: "secret")
 
 publics = ['Tous public', 'Enfants', 'Adolescents', 'Adultes']
 publics.each do |pub|
@@ -27,16 +27,19 @@ categories.each do |category|
 end
 
 
-# 20.times do
-#   Artist.create!(
-#                 name: Faker::Artist.name,
-#                 description: Faker::ChuckNorris.fact,
-#                 address: Faker::Address.street_address,
-#                 hour_rate: (10..100).to_a.sample,
-#                 category_id: (1..7).to_a.sample,
-#                 public_id: (1..4).to_a.sample,
-#                 user_id: user.id
-#                 )
-# end
+20.times do
+  a = Artist.new(
+                name: Faker::Artist.name,
+                description: Faker::ChuckNorris.fact,
+                address: Faker::Address.street_address,
+                hour_rate: (10..100).to_a.sample,
+                category: Category.all.sample,
+                public: Public.all.sample,
+                user_id: user.id
+                )
+  url = "http://static.giantbomb.com/uploads/original/9/99864/2419866-nes_console_set.png"
+  a.remote_photo_url = url
+  a.save
+end
 
 puts 'Created some fake artists...'
